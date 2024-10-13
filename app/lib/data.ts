5476468fba56dcc0f846b7fc39ce78d53ae23cfe
Juan Sala <github@elimar.com>
2024-10-13 06:34:50 +0200

    Chap15_3

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
+    // console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -38,7 +38,7 @@ export async function fetchLatestInvoices() {
       LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => {
-      console.log('Invoice Amount:', invoice.name);
+      // console.log('Invoice Amount:', invoice.name);
       return ({
         ...invoice,
         amount: formatCurrency(invoice.amount),
